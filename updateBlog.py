2f542f6cee4a76f1317230aca348b64513f4332f
Jaiden Muschett <jdm@jaidenm.lan>
2022-05-17 00:49:03 +0100

    added tag file

--- updateBlog.py
+++ updateBlog.py
@@ -1,15 +1,24 @@
 import os
 import json
+from bs4 import BeautifulSoup 
 
-
-articleData = {"articles" : []};
+articleData = {"articles" : [], "taglist" : []};
 ID = 0;
 
-for file in os.listdir('./articles'):
-    articleData["articles"].append({"ID" : ID, "file" : file})
+for filePath in os.listdir('./articles'):
+    row = {"ID" : ID, "file" : filePath};
+    with open('./articles/' + filePath) as file:
+        soup = BeautifulSoup(file.read(),'html.parser');
+        row["date"] = soup.find("meta", attrs={"name":"date"})["content"];
+        row["tags"] = soup.find("meta", attrs={"name":"tags"})["content"].split(" ");
+
+    for t in row["tags"]:
+        if t not in articleData["taglist"]:
+            articleData["taglist"].append(t);
+        
+    articleData["articles"].append(row)
     ID = ID + 1;
 
 print (articleData)
-
 with open('article-db.json', 'w') as f:
-    json.dump(articleData, f)
+   json.dump(articleData, f)
