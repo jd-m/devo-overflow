ce0582e977fdd346e023b7fce6b120690de9ea53
jaiden <jdm@jaidenm.lan>
2022-07-08 22:26:13 +0100

    added card index

--- elisp/publish.el
+++ elisp/publish.el
@@ -3,9 +3,9 @@
 (setq org-publish-project-alist
       '(
 	("posts"
-	 :base-directory "~/jd-m.github.io/"
+	 :base-directory "~/jd-m.github.io/posts/"
 	 :base-extension "org"
-	 :publishing-directory "~/jd-m.github.io/"
+	 :publishing-directory "~/jd-m.github.io/posts/"
 	 :recursive nil
 	 :publishing-function devo-html-publish-to-html
 	 :headline-levels 4
@@ -18,7 +18,7 @@
 	 :base-extension "org_index"
 	 :publishing-directory "~/jd-m.github.io/"
 	 :recursive nil
-	 :publishing-function devo-index-html-publish-to-html
+	 :publishing-function devo-html-publish-to-html
 	 :headline-levels 4
 	 :with-toc nil
 	 :section-numbers nil
